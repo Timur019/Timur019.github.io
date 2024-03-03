741dd5cd5029c442538a6564f4536ee63ea8d4b6
Timur019 <ahmed09081@mail.ru>
2024-03-03 17:02:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttonYes = document.querySelector('.button_yes');
+const buttonNo = document.querySelector('.button_no');
+
+let buttonYesFontSize = 2;
+let buttonNoFontSize = 2;
+
+buttonNo.addEventListener('click', function () {
+    doYesMore();
+});
+
+buttonYes.addEventListener('click', function () {
+    buttonYesPushed();
+});
+
+function doYesMore (){
+    buttonYesFontSize += .3;
+    buttonYes.style.fontSize = buttonYesFontSize + 'em';
+    buttonNoFontSize -= .2;
+    buttonNo.style.fontSize = buttonNoFontSize + 'em';
+}
+
+function buttonYesPushed(){
+    location.href = './page2.html';
+
+}
